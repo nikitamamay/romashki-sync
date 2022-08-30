e577bdc4bd5c81b39aba844af3fb11bf2190e217
Nikita Mamay <nikita@mamay.su>
2022-08-30 13:01:15 +0500

    Enchanced GUI

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from file_watcher import DirectoryWatcher, File
-
+import file_watcher
+File = file_watcher.File
 
 APP_NAME = "RomashkiSync"
 
@@ -25,10 +25,16 @@ class icon():
         return QtGui.QIcon("icons/tick.png")
     def deny():
         return QtGui.QIcon("icons/cross.png")
+    def cancel():
+        return QtGui.QIcon("icons/cancel.png")
     def checkbox_on():
         return QtGui.QIcon("icons/check_box.png")
     def checkbox_off():
         return QtGui.QIcon("icons/check_box_uncheck.png")
+    def magnifier():
+        return QtGui.QIcon("icons/magnifier.png")
+    def update():
+        return QtGui.QIcon("icons/update.png")
 
 
 class Application(QtWidgets.QApplication):
@@ -49,14 +55,14 @@ class ScrollArea(QtWidgets.QScrollArea):
 
 
 class ChangeRepresentationWidget(QtWidgets.QCheckBox):
-    def __init__(self, change_obj: File, parent = None) -> None:
+    def __init__(self, file_obj: File, parent = None) -> None:
         super().__init__(parent)
 
-        self.change_obj = change_obj
+        self.file_obj = file_obj
 
         self.setFont(QtGui.QFont("Consolas"))
-        self.setText(self.change_obj.basename())
-        self.setToolTip(self.change_obj.relpath)
+        self.setText(self.file_obj.basename())
+        self.setToolTip(self.file_obj.relpath)
 
         self.menu = QtWidgets.QMenu(self.text())
         self.menu.addAction(icon.daisy(), self.text())
@@ -85,12 +91,70 @@ class ChangeRepresentationWidget(QtWidgets.QCheckBox):
     #     self.setIgnored(not self.isIgnored())
 
 
+class ChangeRepresentationListWidget(QtWidgets.QWidget):
+    def __init__(self, parent = None) -> None:
+        super().__init__(parent)
+        self.reprs: list[ChangeRepresentationWidget] = []
+
+        self.layout_reprs = QtWidgets.QVBoxLayout()
+        self.layout_reprs.setSpacing(2)
+        self.layout_reprs.setContentsMargins(2, 2, 2, 2)
+        self.layout_reprs.setAlignment(QtCore.Qt.AlignTop)
+
+        self.container_reprs = QtWidgets.QWidget()
+        self.container_reprs.setLayout(self.layout_reprs)
+
+        self.scroll_area = ScrollArea(self.container_reprs)
+
+        self.btn_select_all = QtWidgets.QPushButton(icon.checkbox_on(), "Select all")
+        self.btn_select_all.clicked.connect(lambda: self.set_state_to_all(True))
+
+        self.btn_deselect_all = QtWidgets.QPushButton(icon.checkbox_off(), "Deselect all")
+        self.btn_deselect_all.clicked.connect(lambda: self.set_state_to_all(False))
+
+        self.layout_ = QtWidgets.QGridLayout()
+        self.layout_.setSpacing(2)
+        self.layout_.setContentsMargins(0, 0, 0, 0)
+        self.layout_.addWidget(self.btn_select_all, 0, 0, 1, 1)
+        self.layout_.addWidget(self.btn_deselect_all, 0, 1, 1, 1)
+        self.layout_.addWidget(self.scroll_area, 1, 0, 1, 2)
+        self.setLayout(self.layout_)
+
+    def init_representations(self, files: list[File]) -> None:
+        self.clear_representations()
+        for f in files:
+            self.add_file_repr(f)
+
+    def clear_representations(self) -> None:
+        while len(self.reprs) > 0:
+            self.layout_reprs.removeWidget(self.reprs.pop())
+
+    def add_file_repr(self, f: File) -> None:
+        w = ChangeRepresentationWidget(f)
+        self.layout_reprs.addWidget(w)
+        self.reprs.append(w)
+
+    def set_state_to_all(self, state: bool) -> None:
+        for r in self.reprs:
+            r.setChecked(state)
+
+    def get_checked_files(self) -> list[File]:
+        l: list[File] = []
+        for r in self.reprs:
+            if r.isChecked():
+                l.append(r.file_obj)
+        return l
+
+
 class TrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, parent = None) -> None:
         super().__init__(icon.daisy(), parent)
 
         self.menu = QtWidgets.QMenu("Actions")
-        self.a_exit = QtWidgets.QAction(QtGui.QIcon("icons/cancel.png"), "Exit")
+
+        self.a_raise = QtWidgets.QAction(icon.daisy2(), "Raise")
+        self.menu.addAction(self.a_raise)
+        self.a_exit = QtWidgets.QAction(icon.cancel(), "Exit")
         self.menu.addAction(self.a_exit)
 
         self.setContextMenu(self.menu)
@@ -102,77 +166,92 @@ class MainWindow(QtWidgets.QMainWindow):
     changeSignal = QtCore.pyqtBoundSignal()
     raised = QtCore.pyqtBoundSignal()
 
-    def __init__(self, parent = None) -> None:
+    def __init__(self, config: dict, parent = None) -> None:
         super().__init__(parent)
+
+        self.local_folder_path = config["local_folder_path"]
+        self.gdrive_folder_path = config["gdrive_folder_path"]
+
         self.setMinimumSize(300, 100)
         self.setGeometry(
             self.screen().size().width() - 800 - 20,
             self.screen().size().height() - 325 - 50,
-            800, 325)
+            800,
+            325
+        )
 
         self.tray_icon = TrayIcon(self)
         self.tray_icon.show()
         self.tray_icon.activated.connect(lambda reason: self.toggle() if reason == QtWidgets.QSystemTrayIcon.ActivationReason.Trigger else 0)
         self.tray_icon.messageClicked.connect(self.raiseOnTop)
         self.tray_icon.a_exit.triggered.connect(self.exit)
-
-        a_raise = QtWidgets.QAction(icon.daisy2(), "Raise", self.tray_icon.menu)
-        a_raise.triggered.connect(self.raiseOnTop)
-        self.tray_icon.menu.insertAction(self.tray_icon.menu.actions()[0], a_raise)
+        self.tray_icon.a_raise.triggered.connect(self.raiseOnTop)
 
         self.menubar = QtWidgets.QMenuBar(self)
-        self.menu1 = QtWidgets.QMenu("Actions", self)
+        self.setMenuBar(self.menubar)
+
+        self.menu_sync = QtWidgets.QMenu("Synchronization", self)
+        self.menu_sync.addAction(icon.magnifier(), "Look for changes", self.look_for_changes)
+        self.menu_sync.addAction(icon.update(), "Sync", self.sync)
+        self.menubar.addMenu(self.menu_sync)
+
+        self.menu_window = QtWidgets.QMenu("Window", self)
         self.a_toggle_on_top = QtWidgets.QAction("Always on top", self.menubar)
         self.a_toggle_on_top.setCheckable(True)
         self.a_toggle_on_top.toggled.connect(self.setAlwaysOnTop)  # a_toggle_on_top.isChecked()
-        self.menu1.addAction(self.a_toggle_on_top)
-        self.menu1.addSeparator()
-        self.menu1.addAction(QtGui.QIcon("icons/cancel.png"), "Exit", self.exit)
-        self.menubar.addMenu(self.menu1)
+        self.menu_window.addAction(self.a_toggle_on_top)
+        self.menu_window.addAction(icon.daisy(), "About", self.about)
+        self.menu_window.addSeparator()
+        self.menu_window.addAction(QtGui.QIcon("icons/cancel.png"), "Exit", self.exit)
+        self.menubar.addMenu(self.menu_window)
 
-        self.setMenuBar(self.menubar)
+        self.reprs_list_widget1 = ChangeRepresentationListWidget(self)
+        self.reprs_list_widget2 = ChangeRepresentationListWidget(self)
 
-        self.layout_reprs1 = QtWidgets.QVBoxLayout()
-        self.layout_reprs1.setSpacing(2)
-        self.layout_reprs1.setContentsMargins(2, 2, 2, 2)
-        self.layout_reprs1.setAlignment(QtCore.Qt.AlignTop)
+        self.le_msg = QtWidgets.QLineEdit()
+        self.le_msg.setPlaceholderText("Message...")
 
-        self.layout_reprs2 = QtWidgets.QVBoxLayout()
-        self.layout_reprs2.setSpacing(2)
-        self.layout_reprs2.setContentsMargins(2, 2, 2, 2)
-        self.layout_reprs2.setAlignment(QtCore.Qt.AlignTop)
-
-        self.group_box1 = QtWidgets.QWidget()
-        self.group_box1.setLayout(self.layout_reprs1)
-
-        self.group_box2 = QtWidgets.QWidget()
-        self.group_box2.setLayout(self.layout_reprs2)
-
-        self.scroll_area1 = ScrollArea(self.group_box1)
-        self.scroll_area2 = ScrollArea(self.group_box2)
-
-        self.btn_select_all = QtWidgets.QPushButton(icon.checkbox_on(), "Select all")
-        self.btn_deselect_all = QtWidgets.QPushButton(icon.checkbox_off(), "Deselect all")
-
-        btn_layout = QtWidgets.QHBoxLayout()
-        btn_layout.addWidget(self.btn_select_all)
-        btn_layout.addWidget(self.btn_deselect_all)
-        btn_layout.addStretch(1)
+        self.btn_sync = QtWidgets.QPushButton(icon.update(), "Sync")
+        self.btn_sync.clicked.connect(self.sync)
 
         self.c_widget = QtWidgets.QWidget()
         self.gridlayout = QtWidgets.QGridLayout()
-        self.gridlayout.addLayout(btn_layout, 1, 1, 1, 1)
-        self.gridlayout.addWidget(self.scroll_area1, 2, 1, 1, 1)
-        self.gridlayout.addWidget(self.scroll_area2, 2, 2, 1, 1)
+        self.gridlayout.addWidget(self.reprs_list_widget1, 0, 0, 1, 2)
+        self.gridlayout.addWidget(self.reprs_list_widget2, 0, 2, 1, 2)
+        self.gridlayout.addWidget(self.le_msg, 1, 0, 1, 3)
+        self.gridlayout.addWidget(self.btn_sync, 1, 3, 1, 1)
         self.c_widget.setLayout(self.gridlayout)
 
         self.setCentralWidget(self.c_widget)
 
-        # self.dw: DirectoryWatcher = None
         # self.is_active = True
         # self.startTimer(1000)
 
-    def raiseOnTop(self):
+    def showEvent(self, a0: QtGui.QShowEvent) -> None:
+        super().showEvent(a0)
+        self.look_for_changes()
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        event.ignore()
+        self.hide()
+
+    def setAlwaysOnTop(self, state: bool) -> None:
+        self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint, state)
+        self.a_toggle_on_top.setChecked(state)
+        self.raiseOnTop()
+
+    def about(self) -> None:
+        QtWidgets.QMessageBox.about(
+            self,
+            "About RomashkiSync",
+            """
+            <p><b>RomashkiSync</b> is a file synchronization program.</p>
+            <p>...</p>
+            <p>&copy; Mamay Nikita, 2022.</p>
+            """
+        )
+
+    def raiseOnTop(self) -> None:
         if self.isMaximized():
             self.showMaximized()
         else:
@@ -180,22 +259,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.activateWindow()
         self.raise_()
 
-    def setAlwaysOnTop(self, state: bool) -> None:
-        self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint, state)
-        self.a_toggle_on_top.setChecked(state)
-        self.raiseOnTop()
-
-    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
-        event.ignore()
-        self.hide()
-
-    def toggle(self):
+    def toggle(self) -> None:
         if self.isHidden() or self.isMinimized():
             self.raiseOnTop()
         else:
             self.hide()
 
-    def exit(self):
+    def exit(self) -> None:
         self.close()
         self.exitSignal.emit()
         self.tray_icon.hide()
@@ -205,46 +275,52 @@ class MainWindow(QtWidgets.QMainWindow):
             self.showChanges()
         return super().timerEvent(event)
 
-    def clear(self):
-        for i in range(self.layout_reprs1.count()):
-            self.layout_reprs1.removeWidget(self.layout_reprs1.itemAt(0).widget())
-        for i in range(self.layout_reprs2.count()):
-            self.layout_reprs2.removeWidget(self.layout_reprs2.itemAt(0).widget())
+    def look_for_changes(self) -> None:
+        fc_local = file_watcher.FilesCollection(self.local_folder_path)
+        fc_gdrive = file_watcher.FilesCollection(self.gdrive_folder_path)
 
-    def init_representations(self, l1: list[File], l2: list[File]):
-        self.clear()
-        for f in l1:
-            w = ChangeRepresentationWidget(f, self)
-            self.layout_reprs1.addWidget(w)
-        for f in l2:
-            w = ChangeRepresentationWidget(f, self)
-            self.layout_reprs2.addWidget(w)
+        new_local = file_watcher.FilesCollection.find_newer(fc_local, fc_gdrive)
+        old_local = file_watcher.FilesCollection.find_newer(fc_gdrive, fc_local)
 
-    def get_checked(self) -> tuple[list[File], list[File]]:
-        l1: list[File] = []
-        l2: list[File] = []
+        print("Newer on PC (Upload to cloud):", new_local)
+        print("Older on PC (Remove from cloud OR Download to PC):", old_local)
 
-        for i in range(self.layout_reprs1.count()):
-            w: ChangeRepresentationWidget = self.layout_reprs1.itemAt(i).widget()
-            if w.isChecked():
-                l1.append(w.change_obj)
+        self.reprs_list_widget1.init_representations(new_local)
+        self.reprs_list_widget2.init_representations(old_local)
 
-        for i in range(self.layout_reprs2.count()):
-            w: ChangeRepresentationWidget = self.layout_reprs2.itemAt(i).widget()
-            if w.isChecked():
-                l2.append(w.change_obj)
+    def sync(self) -> None:
+        checked_new_local = self.reprs_list_widget1.get_checked_files()
+        checked_old_local = self.reprs_list_widget2.get_checked_files()
+        print('Checked as "Upload to cloud":', checked_new_local)
+        print('Checked as "Download to PC":', checked_old_local)
 
-        return (l1, l2)
+        if len(checked_new_local) > 0:
+            text_new_local = "<br>".join([f.relpath for f in checked_new_local])
+            if QtWidgets.QMessageBox.question(
+                        self,
+                        "from LOCAL to CLOUD",
+                        f'Are you sure to copy files <br><br><code>{text_new_local}</code><br><br>from "{self.local_folder_path}"<br>to "{self.gdrive_folder_path}"?',
+                        QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
+                    ) == QtWidgets.QMessageBox.Yes:
+                file_watcher.copy_files_array(checked_new_local, self.local_folder_path, self.gdrive_folder_path)
+                print("Copied.")
+            else:
+                print("Abort.")
 
-    # def showChanges(self):
-    #     self.clear()
-    #     to_load, to_delete = self.dw.find_change()
-    #     for f in to_load:
-    #         w = ChangeRepresentationWidget(f)
-    #         self.layout_.addWidget(w)
-    #     for f in to_delete:
-    #         w = ChangeRepresentationWidget(f)
-    #         self.layout_.addWidget(w)
+        if len(checked_old_local) > 0:
+            text_old_local = "<br>".join([f.relpath for f in checked_old_local])
+            if QtWidgets.QMessageBox.question(
+                        self,
+                        "from CLOUD to LOCAL",
+                        f'Are you sure to copy files <br><br><code>{text_old_local}</code><br><br>from "{self.gdrive_folder_path}"<br>to "{self.local_folder_path}"?',
+                        QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
+                    ) == QtWidgets.QMessageBox.Yes:
+                file_watcher.copy_files_array(checked_old_local, self.gdrive_folder_path, self.local_folder_path)
+                print("Copied.")
+            else:
+                print("Abort.")
+
+        self.look_for_changes()
 
 
 if __name__ == "__main__":
@@ -260,16 +336,9 @@ if __name__ == "__main__":
 
     config_reader.read_config_file(sys.argv[1])
 
-
-    fc = file_watcher.FilesCollection(CONFIG["local_folder_path"])
-
-
     app = Application([])
 
-    window = MainWindow()
-
-    window.init_representations(fc.files, fc.files)
-
+    window = MainWindow(CONFIG)
 
     window.raiseOnTop()
 
--- main.py
+++ main.py
@@ -29,57 +29,7 @@ gdrive_folder_path = CONFIG["gdrive_folder_path"]
 
 app = gui.Application([])
 
-window = gui.MainWindow()
-
-
-def look_for_changes():
-    fc_local = file_watcher.FilesCollection(local_folder_path)
-    fc_gdrive = file_watcher.FilesCollection(gdrive_folder_path)
-
-    new_local = file_watcher.FilesCollection.find_newer(fc_local, fc_gdrive)
-    old_local = file_watcher.FilesCollection.find_newer(fc_gdrive, fc_local)
-
-    print("Newer on PC (Upload to cloud):", new_local)
-    print("Older on PC (Remove from cloud OR Download to PC):", old_local)
-
-    window.init_representations(new_local, old_local)
-
-
-
-
-a1 = gui.QtWidgets.QAction("Look for changes")
-a1.triggered.connect(look_for_changes)
-window.menubar.addAction(a1)
-
-
-def f():
-    new_local, old_local = window.get_checked()
-    print('Checked as "Upload to cloud":', new_local)
-    print('Checked as "Download to PC":', old_local)
-
-
-    if len(new_local) > 0:
-        text_new_local = "<br>".join([f.relpath for f in new_local])
-        if gui.QtWidgets.QMessageBox.question(window, "from LOCAL to CLOUD", f'Are you sure to copy files <br><br><code>{text_new_local}</code><br><br>from "{local_folder_path}"<br>to "{gdrive_folder_path}"?', gui.QtWidgets.QMessageBox.Yes | gui.QtWidgets.QMessageBox.No) == gui.QtWidgets.QMessageBox.Yes:
-            file_watcher.copy_files_array(new_local, local_folder_path, gdrive_folder_path)
-            print("Copied.")
-        else:
-            print("Abort.")
-
-    if len(old_local) > 0:
-        text_old_local = "<br>".join([f.relpath for f in old_local])
-        if gui.QtWidgets.QMessageBox.question(window, "from CLOUD to LOCAL", f'Are you sure to copy files <br><br><code>{text_old_local}</code><br><br>from "{gdrive_folder_path}"<br>to "{local_folder_path}"?', gui.QtWidgets.QMessageBox.Yes | gui.QtWidgets.QMessageBox.No) == gui.QtWidgets.QMessageBox.Yes:
-            file_watcher.copy_files_array(old_local, gdrive_folder_path, local_folder_path)
-            print("Copied.")
-        else:
-            print("Abort.")
-
-    look_for_changes()
-
-
-a2 = gui.QtWidgets.QAction("Sync")
-a2.triggered.connect(f)
-window.menubar.addAction(a2)
+window = gui.MainWindow(CONFIG)
 
 # window.raiseOnTop()
 window.show()
