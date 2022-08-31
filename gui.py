4bb88f0d4dcbf7f9c98e2eb1d3da5a3965efb2db
Nikita Mamay <nikita@mamay.su>
2022-08-31 15:14:35 +0500

    last_sync support, enchanced GUI

--- gui.py
+++ gui.py
@@ -35,6 +35,13 @@ class icon():
         return QtGui.QIcon("icons/magnifier.png")
     def update():
         return QtGui.QIcon("icons/update.png")
+    def local():
+        return QtGui.QIcon("icons/computer.png")
+    def cloud():
+        return QtGui.QIcon("icons/network_cloud.png")
+    def cloud_old():
+        return QtGui.QIcon("icons/network_cloud_old.png")
+
 
 
 class Application(QtWidgets.QApplication):
@@ -96,6 +103,8 @@ class ChangeRepresentationListWidget(QtWidgets.QWidget):
         super().__init__(parent)
         self.reprs: list[ChangeRepresentationWidget] = []
 
+        self.pixmap = None
+
         self.layout_reprs = QtWidgets.QVBoxLayout()
         self.layout_reprs.setSpacing(2)
         self.layout_reprs.setContentsMargins(2, 2, 2, 2)
@@ -103,27 +112,38 @@ class ChangeRepresentationListWidget(QtWidgets.QWidget):
 
         self.container_reprs = QtWidgets.QWidget()
         self.container_reprs.setLayout(self.layout_reprs)
+        self.container_reprs.paintEvent = lambda event: self.container_paintEvent(self.container_reprs, event)
 
         self.scroll_area = ScrollArea(self.container_reprs)
+        self.scroll_area.setMinimumHeight(40)
 
-        self.btn_select_all = QtWidgets.QPushButton(icon.checkbox_on(), "Select all")
-        self.btn_select_all.clicked.connect(lambda: self.set_state_to_all(True))
-
-        self.btn_deselect_all = QtWidgets.QPushButton(icon.checkbox_off(), "Deselect all")
-        self.btn_deselect_all.clicked.connect(lambda: self.set_state_to_all(False))
+        self.checkbox_all = QtWidgets.QCheckBox("Select All")
+        self.checkbox_all.setTristate(False)
+        self.checkbox_all.clicked.connect(self.set_state_to_all)
 
         self.layout_ = QtWidgets.QGridLayout()
         self.layout_.setSpacing(2)
         self.layout_.setContentsMargins(0, 0, 0, 0)
-        self.layout_.addWidget(self.btn_select_all, 0, 0, 1, 1)
-        self.layout_.addWidget(self.btn_deselect_all, 0, 1, 1, 1)
-        self.layout_.addWidget(self.scroll_area, 1, 0, 1, 2)
+        self.layout_.addWidget(self.checkbox_all, 1, 0, 1, 1)
+        self.layout_.addWidget(self.scroll_area, 2, 0, 1, 1)
         self.setLayout(self.layout_)
+
+    def container_paintEvent(self, container, a0: QtGui.QPaintEvent) -> None:
+        if self.pixmap:
+            painter = QtGui.QPainter()
+            painter.begin(container)
+            p_w_min = 32
+            margin = min(20, (container.height() - p_w_min) / 2)
+            p_w = max(min(128, container.width(), container.height()) - 2 * margin, p_w_min)
+            painter.drawPixmap(container.width() - p_w - margin, margin, p_w, p_w, self.pixmap)
+            painter.end()
+        return super().paintEvent(a0)
 
     def init_representations(self, files: list[File]) -> None:
         self.clear_representations()
         for f in files:
             self.add_file_repr(f)
+        self.check_tristate()
 
     def clear_representations(self) -> None:
         while len(self.reprs) > 0:
@@ -133,10 +153,12 @@ class ChangeRepresentationListWidget(QtWidgets.QWidget):
         w = ChangeRepresentationWidget(f)
         self.layout_reprs.addWidget(w)
         self.reprs.append(w)
+        w.toggled.connect(self.check_tristate)
 
     def set_state_to_all(self, state: bool) -> None:
         for r in self.reprs:
             r.setChecked(state)
+        self.check_tristate()
 
     def get_checked_files(self) -> list[File]:
         l: list[File] = []
@@ -144,6 +166,18 @@ class ChangeRepresentationListWidget(QtWidgets.QWidget):
             if r.isChecked():
                 l.append(r.file_obj)
         return l
+
+    def check_tristate(self) -> None:
+        if len(self.reprs) > 0:
+            state = self.reprs[0].isChecked()
+            for i in range(1, len(self.reprs)):
+                if self.reprs[i].isChecked() != state:
+                    self.checkbox_all.setCheckState(QtCore.Qt.CheckState.PartiallyChecked)
+                    return
+            self.checkbox_all.setCheckState([
+                QtCore.Qt.CheckState.Unchecked,
+                QtCore.Qt.CheckState.Checked
+            ][int(state)])
 
 
 class TrayIcon(QtWidgets.QSystemTrayIcon):
@@ -169,10 +203,13 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, config: dict, parent = None) -> None:
         super().__init__(parent)
 
-        self.local_folder_path = config["local_folder_path"]
-        self.gdrive_folder_path = config["gdrive_folder_path"]
+        self.config = config
 
-        self.setMinimumSize(300, 100)
+        self.local_folder_path = self.config["local_folder_path"]
+        self.gdrive_folder_path = self.config["gdrive_folder_path"]
+        self.fc_last_sync: file_watcher.FilesCollection = file_watcher.get_last_sync(self.config["files_info_path"], self.gdrive_folder_path)
+
+        self.setMinimumSize(500, 200)
         self.setGeometry(
             self.screen().size().width() - 800 - 20,
             self.screen().size().height() - 325 - 50,
@@ -205,8 +242,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.menu_window.addAction(QtGui.QIcon("icons/cancel.png"), "Exit", self.exit)
         self.menubar.addMenu(self.menu_window)
 
-        self.reprs_list_widget1 = ChangeRepresentationListWidget(self)
-        self.reprs_list_widget2 = ChangeRepresentationListWidget(self)
+        self.reprs_list_widget_newlocal = ChangeRepresentationListWidget(self)
+        self.reprs_list_widget_newlocal.pixmap = icon.local().pixmap(32, 32)
+        self.reprs_list_widget_newgdrive = ChangeRepresentationListWidget(self)
+        self.reprs_list_widget_newgdrive.pixmap = icon.cloud().pixmap(32, 32)
+        self.reprs_list_widget_oldgdrive = ChangeRepresentationListWidget(self)
+        self.reprs_list_widget_oldgdrive.pixmap = icon.cloud_old().pixmap(32, 32)
 
         self.le_msg = QtWidgets.QLineEdit()
         self.le_msg.setPlaceholderText("Message...")
@@ -216,8 +257,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.c_widget = QtWidgets.QWidget()
         self.gridlayout = QtWidgets.QGridLayout()
-        self.gridlayout.addWidget(self.reprs_list_widget1, 0, 0, 1, 2)
-        self.gridlayout.addWidget(self.reprs_list_widget2, 0, 2, 1, 2)
+        self.gridlayout.addWidget(self.reprs_list_widget_newlocal, 0, 0, 1, 2)
+        self.gridlayout.addWidget(self.reprs_list_widget_newgdrive, 0, 2, 1, 2)
+        self.gridlayout.addWidget(self.reprs_list_widget_oldgdrive, 0, 4, 1, 2)
         self.gridlayout.addWidget(self.le_msg, 1, 0, 1, 3)
         self.gridlayout.addWidget(self.btn_sync, 1, 3, 1, 1)
         self.c_widget.setLayout(self.gridlayout)
@@ -279,18 +321,51 @@ class MainWindow(QtWidgets.QMainWindow):
         fc_local = file_watcher.FilesCollection(self.local_folder_path)
         fc_gdrive = file_watcher.FilesCollection(self.gdrive_folder_path)
 
-        new_local = file_watcher.FilesCollection.find_newer(fc_local, fc_gdrive)
-        old_local = file_watcher.FilesCollection.find_newer(fc_gdrive, fc_local)
+        new_local = file_watcher.FilesCollection.find_newer(fc_local, self.fc_last_sync)
+        old_local = file_watcher.FilesCollection.find_newer(self.fc_last_sync, fc_local)
+        new_gdrive = file_watcher.FilesCollection.find_newer(fc_gdrive, self.fc_last_sync)
+        old_gdrive = file_watcher.FilesCollection.find_newer(self.fc_last_sync, fc_gdrive)
 
+        # Is it OK ?
+        # Actually, no: files can be in both new_gdrive and old_local => duplicates
+        new_gdrive.extend(old_local)
+
+        print("---")
         print("Newer on PC (Upload to cloud):", new_local)
-        print("Older on PC (Remove from cloud OR Download to PC):", old_local)
+        print("Newer on Cloud (Download from cloud OR Remove from cloud):", new_gdrive)
+        # print("Older on PC (Download from cloud):", old_local)
+        print("Older on Cloud (Wait for GDrive to download OR Error):", old_gdrive)
+        print("---")
 
-        self.reprs_list_widget1.init_representations(new_local)
-        self.reprs_list_widget2.init_representations(old_local)
+        if len(old_gdrive) > 0:
+            text_oldgdrive = "<br>".join([f.relpath for f in old_gdrive])
+            self.reprs_list_widget_oldgdrive.show()
+            QtWidgets.QMessageBox.warning(
+                self,
+                "Old files detected",
+                f'<p>Some files <br><br><code>{text_oldgdrive}</code><br><br>are older in "{self.gdrive_folder_path}" than they are presented as <code>last_sync</code> state.</p> \
+                <p>The issue may be caused by GDrive still not downloaded new files from cloud. Wait for it.</p> \
+                <p>Or if it is not the case, make sure to threat this error properly.</p>'
+            )
+
+        if len(old_local) > 0:
+            text_oldlocal = "<br>".join([f.relpath for f in old_local])
+            QtWidgets.QMessageBox.warning(
+                self,
+                "Old files detected",
+                f'<p>Some files <br><br><code>{text_oldlocal}</code><br><br>are older in "{self.local_folder_path}" than they are presented as <code>last_sync</code> state.</p> \
+                <p>The issue may be caused by the User not downloaded files that are newer in the cloud.</p> \
+                <p>Or if it is not the case, make sure to threat this error properly.</p>'
+            )
+
+        self.reprs_list_widget_newlocal.init_representations(new_local)
+        self.reprs_list_widget_newgdrive.init_representations(new_gdrive)
+        # self.reprs_list_widget_oldlocal.init_representations(old_local)
+        self.reprs_list_widget_oldgdrive.init_representations(old_gdrive)
 
     def sync(self) -> None:
-        checked_new_local = self.reprs_list_widget1.get_checked_files()
-        checked_old_local = self.reprs_list_widget2.get_checked_files()
+        checked_new_local = self.reprs_list_widget_newlocal.get_checked_files()
+        checked_old_local = self.reprs_list_widget_newgdrive.get_checked_files()
         print('Checked as "Upload to cloud":', checked_new_local)
         print('Checked as "Download to PC":', checked_old_local)
 
@@ -302,8 +377,9 @@ class MainWindow(QtWidgets.QMainWindow):
                         f'Are you sure to copy files <br><br><code>{text_new_local}</code><br><br>from "{self.local_folder_path}"<br>to "{self.gdrive_folder_path}"?',
                         QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
                     ) == QtWidgets.QMessageBox.Yes:
-                file_watcher.copy_files_array(checked_new_local, self.local_folder_path, self.gdrive_folder_path)
-                print("Copied.")
+                files_copied = file_watcher.copy_files_array(checked_new_local, self.local_folder_path, self.gdrive_folder_path)
+                print("Copied from LOCAL to CLOUD:", files_copied)
+                self.fc_last_sync.update_partially(files_copied)
             else:
                 print("Abort.")
 
@@ -315,10 +391,13 @@ class MainWindow(QtWidgets.QMainWindow):
                         f'Are you sure to copy files <br><br><code>{text_old_local}</code><br><br>from "{self.gdrive_folder_path}"<br>to "{self.local_folder_path}"?',
                         QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
                     ) == QtWidgets.QMessageBox.Yes:
-                file_watcher.copy_files_array(checked_old_local, self.gdrive_folder_path, self.local_folder_path)
-                print("Copied.")
+                files_copied = file_watcher.copy_files_array(checked_old_local, self.gdrive_folder_path, self.local_folder_path)
+                print("Copied from CLOUD to LOCAL:", files_copied)
+                self.fc_last_sync.update_partially(files_copied)
             else:
                 print("Abort.")
+
+        file_watcher.save_last_sync(self.config["files_info_path"], self.fc_last_sync)
 
         self.look_for_changes()
 
